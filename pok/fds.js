298f1be082e8913f45688b7eefbae9126ce0200b
tureti123 <132614552+tureti123@users.noreply.github.com>
2024-08-08 16:44:48 +0200

    Add files via upload

--- pok/fds.js
+++ pok/fds.js
@@ -1,5 +1,5 @@
 const http=require("http")
-const server=http.createServer((res,req)=>{
+const server=http.createServer((req,res)=>{
     res.writeHead(200,{"Content-type":"text/plain"})
     res.end("hello world")
 })
